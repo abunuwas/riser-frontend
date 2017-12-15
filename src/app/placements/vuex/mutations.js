ebc9592e4e347b1fbfa1d648c6d93009f1fe388c
abunuwas <joseharoperalta@gmail.com>
2017-12-15 08:06:27 +0000

    Update mutations

--- src/app/placements/vuex/mutations.js
+++ src/app/placements/vuex/mutations.js
@@ -1,5 +1,19 @@
+import Vue from 'vue'
+
 export default {
   CREATE_PLACEMENT (state, payload) {
     state.placements[payload.placement.id] = payload.placement
+  },
+
+  UPDATE_PLACEMENT (state, payload) {
+    state.placements[payload.placement.id] = payload.placement
+  },
+
+  LOAD_PLACEMENTS (state, payload) {
+    state.placements = payload
+  },
+
+  DELETE_PLACEMENT (state, payload) {
+    Vue.delete(state.placements, payload.placement.id)
   }
 }
