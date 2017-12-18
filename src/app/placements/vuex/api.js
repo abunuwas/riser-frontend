e095deda921331d7c2e15afc696c30d1129e9b3e
abunuwas <joseharoperalta@gmail.com>
2017-12-18 12:03:50 +0000

    Implement removePlacement function

--- src/app/placements/vuex/api.js
+++ src/app/placements/vuex/api.js
@@ -3,6 +3,7 @@ import moment from 'moment'
 
 export const savePlacement = (placement) => {
   placement = Object.assign({}, placement)
+  console.log(placement)
   placement.start = placement.start.toJSON()
   placement.end = placement.end.toJSON()
 
@@ -18,17 +19,25 @@ export const fetchPlacements = (user, token) => {
   return axios.get('http://localhost:7000/api/placements', { user, token })
     .then((res) => {
       let placements = res.data.placements
-      Object.keys(placements).forEach((o) => {
-        placements[o].start = new Date(moment(placements[o].start).format('YYYY-MM-DD'))
-        placements[o].end = new Date(moment(placements[o].end).format('YYYY-MM-DD'))
+      Object.keys(placements).forEach((index) => {
+        placements[index].start = new Date(
+          moment(placements[index].start).format('YYYY-MM-DD'))
+        placements[index].end = new Date(
+          moment(placements[index].end).format('YYYY-MM-DD'))
       })
-      console.log('fetching placements...')
       console.log(placements)
-      console.log(placements[0])
       return placements
     })
 }
 
-export const removePlacement = (user, token) => {
-  console.log('haha')
+export const removePlacement = (placement) => {
+  console.log(placement.id)
+  return axios.delete('http://localhost:7000/api/placements/' + placement.id)
+    .then((res) => {
+      console.log(res)
+      return true
+    }).catch((err) => {
+      console.log(err)
+      return false
+    })
 }
