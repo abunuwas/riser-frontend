aef7c9d0c519a1729a32868f40d60ff421896658
abunuwas <joseharoperalta@gmail.com>
2018-01-03 07:16:13 +0000

    Style timesheets list view

--- src/app/timesheets/components/ListTimesheets.vue
+++ src/app/timesheets/components/ListTimesheets.vue
@@ -1,17 +1,22 @@
 <template>
-  <div id="timesheets-list-view">
-    <div v-if="timesheetsNotDefined()">
-      <label for="from">From</label>
-      <p class="control">
-        <input
-          type="number"
-          class="input"
-          name="numberOfTimeSheets"
-          v-model="numberOfTimesheets"
-        >
-      </p>
-      <pre>{{ numberOfTimesheets }}</pre>
-    </div>
+  <div class="columns">
+    <div class="column is-2"></div>
+      <div id="timesheets-ist-view">
+        <nav class="level">
+          <div class="level-left">
+            <h2 class="title is-2">Timesheets</h2>
+          </div>
+        </nav>
+        <!--<pre>{{ timesheets }}</pre>-->
+        <div class="container">
+          <div class="item"
+               v-for="timesheet, key in timesheets">
+            <div v-bind:style="{ 'background-color': getColor(timesheet.status) }">{{ timesheet.status }}</div>
+            <div>{{ timesheet.start }}-{{ timesheet.end }}</div>
+            <div>{{ timesheet.working_days }} days worked</div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -29,7 +34,56 @@
 
     data: () => {
       return {
-        numberOfTimesheets: 0
+        timesheets: {
+          '1': {
+            start: '2017-01-03',
+            end: '2017-02-04',
+            status: 'approved',
+            working_days: 10
+          },
+          '2': {
+            start: '2017-01-03',
+            end: '2017-02-04',
+            status: 'approved',
+            working_days: 10
+          },
+          '3': {
+            start: '2017-01-03',
+            end: '2017-02-04',
+            status: 'approved',
+            working_days: 10
+          },
+          '4': {
+            start: '2017-01-03',
+            end: '2017-02-04',
+            status: 'approved',
+            working_days: 10
+          },
+          '5': {
+            start: '2017-01-03',
+            end: '2017-02-04',
+            status: 'approved',
+            working_days: 10
+          },
+          '6': {
+            start: '2017-01-03',
+            end: '2017-02-04',
+            status: 'approved',
+            working_days: 10
+          },
+          '7': {
+            start: '2017-01-03',
+            end: '2017-02-04',
+            status: 'approved',
+            working_days: 10
+          },
+          '8': {
+            start: '2017-04-03',
+            end: '2017-05-04',
+            status: 'undefined',
+            working_days: 0
+          }
+        }
       }
     },
 
@@ -44,8 +98,12 @@
       ...mapActions([
       ]),
 
-      timesheetsNotDefined () {
-        return this.numberOfTimesheets === 0 || this.numberOfTimesheets === ''
+      getColor (status) {
+        if (status === 'approved') {
+          return 'green'
+        } else {
+          return 'blue'
+        }
       }
     },
 
@@ -55,3 +113,20 @@
     }
   }
 </script>
+
+<style>
+  .container {
+    width: 100%;
+    height: 100%;
+    border: black .1px solid;
+    border-bottom: black .1px solid;
+    border-right: black .1px solid;
+  }
+
+  .item {
+    float: left;
+    width: 30%;
+    height: 28%;
+    border: red .2px solid;
+  }
+</style>
