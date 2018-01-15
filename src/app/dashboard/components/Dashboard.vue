980a91d53a1f38a697f906b86a3dff7f3795332c
abunuwas <joseharoperalta@gmail.com>
2018-01-15 21:20:07 +0000

    Style better the dashboard and add surprise :D

--- src/app/dashboard/components/Dashboard.vue
+++ src/app/dashboard/components/Dashboard.vue
@@ -4,30 +4,46 @@
     <div class="dashboard">
       <h2 class="title is-2 has-text-centered">Your dashboard</h2>
       <nav class="level">
-        <aside class="menu">
+        <aside class="menu" style="background-color: #f2f2f2">
           <p class="menu-label">Sections</p>
           <ul class="menu-list">
             <li>
               <router-link
-              class="button is-primary"
               :to="{ name: 'listPlacements' }"
             >Placements
               </router-link>
             </li>
             <li>
               <router-link
-                class="button is-primary"
                 :to="{ name: 'listTimesheets' }"
               >Timesheets</router-link>
+            </li>
+            <p class="menu-label">Rock n' Roll</p>
+          </ul>
+          <ul class="menu-list">
+            <li>
+              <a v-on:click="showMusic()"
+              >Rock n' Roll</a>
             </li>
           </ul>
         </aside>
       </nav>
     </div>
+    <div class="column is-8">
+    <iframe src="http://www.youtube.com/embed/M6JpxDebokM?autoplay=1" hidden="false" id="player"
+            width="560" height="315" frameborder="0" allowfullscreen></iframe>
+    </div>
   </div>
 </template>
 <script>
   export default {
-    name: 'dashboard'
+    name: 'dashboard',
+
+    methods: {
+      showMusic () {
+        let musicFrame = document.getElementById('player')
+        musicFrame.removeAttribute('hidden')
+      }
+    }
   }
 </script>
