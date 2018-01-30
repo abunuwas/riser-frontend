8e69878e52ca75e1cb73e2045e7c354719399315
abunuwas <joseharoperalta@gmail.com>
2018-01-30 17:46:24 +0000

    Add custom headers

--- src/app/timesheets/vuex/api.js
+++ src/app/timesheets/vuex/api.js
@@ -9,7 +9,16 @@ export const saveTimesheet = (timesheet) => {
   console.log('TIMESHEET start: ' + timesheet.start)
   let idURL = timesheet.id.split('-')[1]
   let url = `http://82.223.35.243/2/2/${idURL}/riserts.fcgi`
-  return axios.post(url, { 'timesheet': timesheet, headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+  let config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+  return axios.post(
+    url,
+    { data: { 'timesheet': timesheet } },
+    config
+    )
     .then((res) => {
       console.log('RESPONSE FROM SERVER: ' + res)
       return res.data
